5532b5cadb85283d20ab66d98d40496e26923329
djperrefort <djperrefort@pitt.edu>
2019-07-29 11:03:41 -0400

    Outline logic for tabulating chisq results

--- analysis/spectra_chisq.py
+++ analysis/spectra_chisq.py
@@ -3,9 +3,14 @@
 
 """Calculate the chi-squared values for modeled spectra"""
 
-import numpy as np
+from copy import deepcopy
 
-from .utils import chisq_sum
+import numpy as np
+from astropy.table import Table
+from scipy.ndimage.filters import gaussian_filter1d
+from sndata.csp import dr1  # Todo: Switch to dr2
+
+from .utils import chisq_sum, get_spectra_for_id, make_pbar
 
 
 # Todo: Write the color_warp_spectrum function
@@ -23,6 +28,29 @@ def color_warp_spectrum(wave, flux, flux_err, **kwargs):
     """
 
     return flux, flux_err
+
+
+def filter_spectrum(flux, width):
+    """Reduce the resolution of a spectrum using a Gaussian filter
+
+    Uses a filter with the given width and a sigma of 1.
+
+    Args:
+        flux (ndarray): An array of flux values
+        width    (int): Width of the
+
+    Returns:
+        An array of flux values
+        An array of error values for the fluxes
+    """
+
+    if not isinstance(width, int):
+        raise TypeError('Width must be an int')
+
+    # See https://stackoverflow.com/questions/25216382/gaussian-filter-in-scipy
+    truncate = (width - 1) / 2
+    flux_gauss = gaussian_filter1d(flux, 1, truncate=truncate)
+    return flux_gauss
 
 
 def band_chisq_spectrum(wave, flux, flux_err, model_flux, band_ranges):
@@ -72,3 +100,60 @@ def band_chisq_spectra(wave, flux, flux_err, model_flux, band_ranges):
             out_data[band].append(chisq)
 
     return out_data
+
+
+def create_empty_output_table(model, band_names):
+    """Create an empty astropy table for storing chi-squared results
+
+    Args:
+        model      (Model): sncosmo model the table will be used for
+        band_names  (list): List band names
+
+    Returns:
+        An astropy table
+    """
+
+    names, dtype = ['obj_id'], ['U100']
+    names.extend(band_names)
+    dtype.extend((float for _ in band_names))
+
+    out_table = Table(names=names, dtype=dtype, masked=True)
+    out_table.meta['source'] = model.source.name
+    out_table.meta['version'] = model.source.version
+    return out_table
+
+
+def tabulate_chi_squared(data_release, model, band_ranges, verbose=True):
+    """Tabulate color residuals for a given model
+
+    Args:
+        data_release (module): An sndata data release
+        model         (Model): An sncosmo model
+        band_ranges    (dict): A dictionary of band pass limits.
+        verbose        (bool): Whether to display a progress bar
+    """
+
+    model = deepcopy(model)
+    band_ranges = deepcopy(band_ranges)
+
+    # Construct iterator over object ids
+    id_pbar = make_pbar(
+        data_release.get_available_ids(), verbose=verbose, desc='Targets')
+
+    out_table = create_empty_output_table(model, band_ranges.keys())
+    for obj_id in id_pbar:
+        obs_data = zip(*get_spectra_for_id(dr1, obj_id))
+        for time, wave, flux, flux_err in obs_data:
+            obs_flux, obs_err = color_warp_spectrum(wave, flux, flux_err)
+
+            # Todo: Define ebv and width
+            model.set(ebv=ebv)
+            model_flux = filter_spectrum(model.flux(time, wave), width)
+
+            chi_sq = band_chisq_spectrum(
+                wave, obs_flux, obs_err, model_flux, band_ranges)
+
+            band_ranges['obj_id'] = obj_id
+            out_table.add_row(band_ranges)
+
+    return out_table
--- analysis/utils.py
+++ analysis/utils.py
@@ -7,6 +7,32 @@ import numpy as np
 from tqdm import tqdm
 
 
+def get_spectra_for_id(data_release, obj_id):
+    """Get spectral data for a given data id
+
+    Args:
+        data_release (module): An sndata data access module
+        obj_id          (str): The ID of the desired object
+
+    Returns:
+        A list of observed MJD dates for each spectra
+        A 2d list of wavelength values for each date
+        A 2d list of flux values for each date
+    """
+
+    data = data_release.get_data_for_id(obj_id)
+    obs_dates = list(set(data['date']))
+
+    wavelength, flux, flux_err = [], [], []
+    for date in obs_dates:
+        data_for_date = data[data['date'] == date]
+        wavelength.append(data_for_date['wavelength'])
+        flux.append(data_for_date['flux'])
+        flux_err.append(.1 * data_for_date['flux'])  # Todo: Parse flux errors
+
+    return obs_dates, wavelength, flux, flux_err
+
+
 def make_pbar(iterable, verbose, **kwargs):
     """A wrapper for tqdm.tqdm
 
