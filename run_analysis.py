0a634c45bc7a0c3aec0d67cd777d33104a64dd3c
djperrefort <djperrefort@pitt.edu>
2019-07-30 14:43:28 -0400

    Updates CLI

--- run_analysis.py
+++ run_analysis.py
@@ -7,39 +7,43 @@ import argparse
 from pathlib import Path
 
 import sncosmo
-from sndata.csp import dr3
+from sndata.csp import dr1, dr3
 from tqdm import tqdm
 
+from analysis import equivalent_width
 from analysis import lc_colors
 from analysis import models
 
 models.register_sources()
+dr1.download_module_data()
 dr3.download_module_data()
 dr3.register_filters()
 
 
-def get_models(cli_args):
-    """Return a list of SNCosmo models specified by command line arguments
+def get_models(model_id_list):
+    """Return a list of models corresponding to a list of model ids
+
+    Model ids should be of the form ``<source name>,<source version>
 
     Args:
-        cli_args (argparse.Namespace): Command line arguments
+        model_id_list (list): A list of model ids
 
     Returns:
-        A list of SNCosmo models
+        A list of sncosmo Models
     """
 
-    models_list = []
-    for name, version in zip(cli_args.models, cli_args.versions):
-        source = sncosmo.get_source(name, version=version)
-        model = sncosmo.Model(source=source)
+    model_list = []
+    for model_id in model_id_list:
+        name, version = model_id.split(',')
+        model = sncosmo.Model(sncosmo.get_source(name, version=version))
         model.add_effect(sncosmo.F99Dust(), 'ext', 'rest')
-        models_list.append(model)
+        model_list.append(model)
 
-    return models_list
+    return model_list
 
 
 def run_lc_color(cli_args):
-    """Run light curve fits using command line args
+    """Run color analysis
 
     Args:
         cli_args (argparse.Namespace): Command line arguments
@@ -57,11 +61,10 @@ def run_lc_color(cli_args):
         'csp_dr3_H',
     ]
 
-    color_combos = [(unique_bands[i], unique_bands[i + 1]) for i in
-                    range(len(unique_bands) - 1)]
-
+    color_combos = [(unique_bands[i], unique_bands[i + 1]) for i in range(len(unique_bands) - 1)]
     out_dir = Path(cli_args.out_dir) / 'color_evolution'
-    for model in get_models(cli_args):
+    tqdm.write('Tabulating color evolution')
+    for model in get_models(cli_args.models):
         tqdm.write(f'Fitting {model.source.name} {model.source.version}')
         file_name = f'{model.source.name}_{model.source.version}.ecsv'
         t = lc_colors.tabulate_residuals(dr3, model, color_combos)
@@ -69,39 +72,60 @@ def run_lc_color(cli_args):
         tqdm.write('\n')
 
 
+def run_ew(cli_args):
+    """Run equivalent width analysis
+
+    Args:
+        cli_args (argparse.Namespace): Command line arguments
+    """
+
+    start_msg = 'Tabulating equivalent widths (Fixed bounds = {})'
+    tqdm.write(start_msg.format(cli_args.fix_boundaries))
+
+    ew_results = equivalent_width.tabulate_pew(
+        data_release=dr1,
+        models=get_models(cli_args.models),
+        fix_boundaries=cli_args.fix_boundaries)
+
+    out_dir = Path(cli_args.out_dir) / 'equivalent_width'
+    out_dir.mkdir(parents=True, exist_ok=True)
+    ew_results.write(out_dir / f'fixed_{cli_args.fix_boundaries}.ecsv')
+
+
 # Parse command line input
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Compare various SN models against CSP data.')
-
+    parser = argparse.ArgumentParser(description='Compare various SN models against CSP data.')
     subparsers = parser.add_subparsers(help='')
-    color_parser = subparsers.add_parser(
-        'lc_color', help='Compare color evolution with models.')
 
-    color_parser.set_defaults(func=run_lc_color)
-    color_parser.add_argument(
-        '-m', '--models',
-        type=str,
-        nargs='+',
-        default=['salt2'],
-        help='Models to fit')
-
-    color_parser.add_argument(
-        '-v', '--versions',
-        type=str,
-        nargs='+',
-        default=['2.4'],
-        help='Version of each model to fit')
-
-    color_parser.add_argument(
+    parser.add_argument(
         '-o', '--out_dir',
         type=str,
         default=['./'],
         help='Output directory')
 
-    cli_args = parser.parse_args()
-    if not (len(cli_args.models) == len(cli_args.versions)):
-        raise ValueError(
-            'Number of models and versions must match.')
+    color_parser = subparsers.add_parser('lc_color', help='Compare color evolution with models.')
+    color_parser.set_defaults(func=run_lc_color)
+    color_parser.add_argument(
+        '-m', '--models',
+        type=str,
+        nargs='+',
+        default=['salt,2.4'],
+        help='Models to use')
 
+    ew_parser = subparsers.add_parser('equivalent_width', help='Calculate pseudo equivalent width values')
+    ew_parser.set_defaults(func=run_ew)
+    ew_parser.add_argument(
+        '-f', '--fix_boundaries',
+        type=bool,
+        default=False,
+        help='Fix feature bounds to measured values')
+
+    ew_parser.add_argument(
+        '-m', '--models',
+        type=str,
+        nargs='+',
+        default=['salt,2.4'],
+        help='Models to use')
+
+    cli_args = parser.parse_args()
     cli_args.func(cli_args)
