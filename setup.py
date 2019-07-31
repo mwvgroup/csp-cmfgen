93ebbd49e82c618f185dc7d606fecd41e9829642
djperrefort <djperrefort@pitt.edu>
2019-07-31 09:32:47 -0400

    Adds package data to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ from setuptools import find_packages, setup
 
 setup(name='nir_analysis',
       packages=find_packages(),
-      python_requires='>=3.6',
+      python_requires='>=3.7',
       setup_requires=['pytest-runner'],
-      tests_require=['pytest'])
+      tests_require=['pytest'],
+      package_data={'': ['features.yml']},
+      include_pacakge_data=True)
