5dd1e43dc8ee598c2b3c634ff68dea9ea95d7513
Megan Splettstoesser <megan_splettstoesser@mc-wifi-10-215-106-235.wireless.pitt.edu>
2019-10-01 13:39:43 -0400

    Include make table

--- analysis/spectra_chisq/__init__.py
+++ analysis/spectra_chisq/__init__.py
@@ -8,9 +8,12 @@ spectra.
 """
 
 from ._chisq import calc_chisq, band_limits, tabulate_chisq
+from .photometry_comparison import photometry_to_spectra_time, tabulate_synthetic_photometry
 
 __all__ = [
     'band_limits',
     'calc_chisq',
-    'tabulate_chisq'
+    'tabulate_chisq',
+    #'photometry_to_spectra_time',
+    #'tabulate_synthetic_photometry'
 ]
