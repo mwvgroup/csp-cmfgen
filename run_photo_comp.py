b0a73b20a9d5b2c0d9d8467e4bbc8369aaef666a
Megan Splettstoesser <megan_splettstoesser@mc-wifi-10-215-106-235.wireless.pitt.edu>
2019-10-01 21:48:06 -0400

    Move make table to photo_comparison.py

--- run_photo_comp.py
+++ run_photo_comp.py
@@ -10,25 +10,11 @@ dr3.download_module_data()
 dr3.register_filters(force=True)
 
 
-def make_table():
-    """Create photometry comparison table
-    """
+def run_make_table():
 
-    # Get synthetic photometry for all spectroscopic data
-    synthetic_photo_table = vstack([spectra_chisq.photometry_comparison.tabulate_synthetic_photometry(data) for data in dr1.iter_data(verbose=True)])
-    # Regress all photometric data 
-    photo_table = spectra_chisq.photometry_comparison.photometry_to_spectra_time(spec_data=synthetic_photo_table)
-    # Create output table
-    tbl = join(synthetic_photo_table, photo_table, join_type='left')
-    # TODO display filename at prompt?
-    ans = input('Overwrite joint table? (y/n)')
-    if ans == 'y':
-        tbl.write('../../../../comp.fits', overwrite=True)
-    if ans == 'n':
-        # TODO also input path?
-        fn = input('New filename (ex: table_name) :')
-        tbl.write('../../../../{}.fits'.format(fn))
-    
-    return None
+    spectra_chisq.photometry_comparison.make_table()
 
-make_table()
+
+# TODO add plotting code?
+
+run_make_table()
