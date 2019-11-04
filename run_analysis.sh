62c87cc66aaff21b335ba91db4af965cdb6b2227
djperrefort <djperrefort@pitt.edu>
2019-11-04 10:55:18 -0500

    Fixes arg typo

--- run_analysis.sh
+++ run_analysis.sh
@@ -9,4 +9,4 @@ python run_analysis.py -o results color_chisq -m CMFGEN,1.04 CMFGEN,1.02 CMFGEN,
 python run_analysis.py -o results color_chisq -m CMFGEN,1.04 CMFGEN,1.02 CMFGEN,1.4 CMFGEN,1.7 -s 5 -e 50;
 python run_analysis.py -o results spec_chisq -m CMFGEN,1.04 CMFGEN,1.02 CMFGEN,1.4 CMFGEN,1.7 -e .03 -t .1 -b csp_dr3_u csp_dr3_g csp_dr3_r csp_dr3_i csp_dr3_B csp_dr3_V csp_dr3_Y csp_dr3_J csp_dr3_H;
 python run_analysis.py -o results spec_chisq -m CMFGEN,1.04 CMFGEN,1.02 CMFGEN,1.4 CMFGEN,1.7 -e .03 -f pW1 pW2 pW3 pW4 pW5 pW6 pW7 pW8;
-python run_analysis.py -o results synth_phot -m CMFGEN,1.04 CMFGEN,1.02 CMFGEN,1.4 CMFGEN,1.7 -e 0.03
+python run_analysis.py -o results synth_phot -e 0.03
